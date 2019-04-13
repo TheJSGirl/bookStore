a5d97bd38ba29f47e5d8bb485b1ad59976c3abed
TheJSGirl <prinegi100@gmail.com>
2019-04-13 12:33:12 +0530

    fix: correct the path for contollers

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const mainRoutes = require('./src/modules/index');
 const app = express();
 const bodyParser = require('body-parser');
+const path =require('path');
+const fs = require('fs')
 app.use(bodyParser.json());
 
 mainRoutes(app);
@@ -10,7 +12,37 @@ mainRoutes(app);
 app.get('/', (req, res) => {
     console.log("here");
 })  
+console.log(path.join(__dirname, './modules/books/controllers'));
+var swaggerTools = require('swagger-tools');
+var jsyaml = require('js-yaml');
+console.log( path.join(__dirname, './modules/books/controllers'))
+// swaggerRouter configuration
+var options = {
+  swaggerUi: path.join(__dirname, '/swagger.json'),
+  controllers: path.join(__dirname, '/src/modules/books/'),
+  useStubs: process.env.NODE_ENV === 'development' // Conditionally turn on stubs (mock mode)
+};
+
+// The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
+var spec = fs.readFileSync(path.join(__dirname,'./src/swagger.yaml'), 'utf8');
+var swaggerDoc = jsyaml.safeLoad(spec);
+
+// Initialize the Swagger middleware
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+
+  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+  app.use(middleware.swaggerMetadata());
+
+  // Validate Swagger requests
+  app.use(middleware.swaggerValidator());
+
+  // Route validated requests to appropriate controller
+  app.use(middleware.swaggerRouter(options));
+
+  // Serve the Swagger documents and Swagger UI
+  app.use(middleware.swaggerUi());
 
 app.listen(3000, (req, res) => {
     console.log(`listing at port 3000`)
+})
 })
--- src/modules/books/controllers.js
+++ src/modules/books/controllers.js
@@ -1,10 +1,12 @@
 const Book = require('./models');
 
 async function get(req, res){
+    // eslint-disable-next-line no-console
     console.log('book store')
 }
 
 async function post(req, res) {
+    // eslint-disable-next-line no-unused-vars
     const {user, name, author, edition } = req.body;
     const data = {
         name, 
