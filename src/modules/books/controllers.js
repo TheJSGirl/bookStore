0bda05b045a673061a25d43d3723e9d443437693
rama41222 <dinushankanrg@gmail.com>
2019-07-07 04:52:19 +0530

    fix: removed deleted books being listed

--- src/modules/books/controllers.js
+++ src/modules/books/controllers.js
@@ -4,13 +4,13 @@ const { response } = require('./../../utils');
 async function list(req, res) {
   const limit = parseInt(req.query.limit, 10) || 10;
   const skip = parseInt(req.query.skip, 10) || 0;
-  const books = await Book.find().skip(skip).limit(limit);
+  const books = await Book.find({ deleted: false }).skip(skip).limit(limit);
   res.status(200).send(response(books, '', true));
 }
 
 async function listOne(req, res) {
   const { id } = req.params;
-  const book = await Book.findOne({ _id: id });
+  const book = await Book.findOne({ _id: id, deleted: false });
   res.status(200).json(response(book, '', true));
 }
 
