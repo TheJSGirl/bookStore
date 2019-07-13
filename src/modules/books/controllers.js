cc8cce16ef2932b3924f6f4c769daa53cb7028b5
rama41222 <dinushankanrg@gmail.com>
2019-07-13 11:46:14 +0530

    feat: listmybooks controller

--- src/modules/books/controllers.js
+++ src/modules/books/controllers.js
@@ -8,6 +8,13 @@ async function list(req, res) {
   res.status(200).send(response(books, '', true));
 }
 
+async function listMyBooks(req, res) {
+  const limit = parseInt(req.query.limit, 10) || 10;
+  const skip = parseInt(req.query.skip, 10) || 0;
+  const books = await Book.find({ deleted: false, user: req.user._id }).skip(skip).limit(limit);
+  res.status(200).send(response(books, '', true));
+}
+
 async function listOne(req, res) {
   const { id } = req.params;
   const book = await Book.findOne({ _id: id, deleted: false });
@@ -20,15 +27,38 @@ async function create(req, res) {
   //   return res.status(401).send(response({}, 'Unauthorized request', false));
   // }
   const book = await Book
-    .create({ ...req.body });
+    .create({ ...req.body, user: req.user._id });
   if (!book) {
     return res.status(400).send(response({}, 'Something went wrong', false));
   }
   res.status(200).send(response(book, '', true));
 }
 
+async function edit(req, res) {
+  const { id } = req.params;
+  const book = await Book
+    .findOneAndUpdate({ $and: [{ _id: id }, { user: req.user._id }] }, { ...req.body }, { new: true });
+  if (!book) {
+    return res.status(400).send(response({}, 'Something went wrong', false));
+  }
+  res.status(200).send(response(book, 'Successfully Updated the book', true));
+}
+
+async function remove(req, res) {
+  const { id } = req.params;
+  const book = await Book
+    .findOneAndUpdate({ _id: id }, { deleted: true }, { new: true });
+  if (!book) {
+    return res.status(400).send(response({}, 'Something went wrong', false));
+  }
+  res.status(200).send(response({}, 'Successfully deleted the book', true));
+}
+
 module.exports = {
   list,
   listOne,
   create,
+  remove,
+  edit,
+  listMyBooks
 };
